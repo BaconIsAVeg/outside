22bd9f7e3297e8e64c3bfb349749751ca7087b2f
BaconIsAVeg <baconisaveg@fastmail.ca>
2025-06-24 11:20:05 -0600

    feat: Add a custom teplate formatter `| round` to round numbers in templates - remove the previous `_round` context fields - bump version to 0.3.0

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -6,9 +6,19 @@ pub mod waybar;
 use crate::context::Context;
 use crate::Settings;
 
+use std::fmt::Write;
+
 pub trait Output {
     fn new(context: Context, settings: Settings) -> Self;
     fn render(&self) -> String;
+    fn tt() -> tinytemplate::TinyTemplate<'static> {
+        let mut tt = tinytemplate::TinyTemplate::new();
+        tt.add_formatter("round", |value, output| {
+            write!(output, "{:.0}", value.as_f64().unwrap_or(0.0).round())?;
+            Ok(())
+        });
+        tt
+    }
 }
 
 // TODO: Figure out how this works
