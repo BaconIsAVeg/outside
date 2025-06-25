b148f2140e4ed8dc66ab4b701575abd6559bce63
BaconIsAVeg <baconisaveg@fastmail.ca>
2025-06-25 01:02:19 -0600

    refactor: adding release build workflow

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -7,12 +7,13 @@ use crate::context::Context;
 use crate::Settings;
 
 use std::fmt::Write;
+use tinytemplate::TinyTemplate;
 
 pub trait Output {
     fn new(context: Context, settings: Settings) -> Self;
     fn render(&self) -> String;
-    fn tt() -> tinytemplate::TinyTemplate<'static> {
-        let mut tt = tinytemplate::TinyTemplate::new();
+    fn tt() -> TinyTemplate<'static> {
+        let mut tt = TinyTemplate::new();
         tt.add_formatter("round", |value, output| {
             write!(output, "{:.0}", value.as_f64().unwrap_or(0.0).round())?;
             Ok(())
