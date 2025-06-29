af66ecf6d4e1fca19ecc1b7c48f122e204efeecf
BaconIsAVeg <baconisaveg@fastmail.ca>
2025-06-29 09:16:12 -0600

    fix(#1): Fix geolocation API response handling to use `name` instead of `admin2`

--- src/api/geolocation.rs
+++ src/api/geolocation.rs
@@ -12,7 +12,7 @@ pub struct GeoLocation {
 
 #[derive(serde::Deserialize, serde::Serialize, Debug)]
 pub struct Results {
-    pub admin2: String,
+    pub name: String,
     pub country_code: String,
     pub latitude: f64,
     pub longitude: f64,
@@ -21,7 +21,8 @@ pub struct Results {
 impl Location for GeoLocation {
     fn fetch(n: &str, c: &str) -> LocationData {
         let base_url = "https://geocoding-api.open-meteo.com/v1/search";
-        let params = vec![("name", n), ("countryCode", c)];
+        let params =
+            vec![("name", n), ("countryCode", c), ("count", "1"), ("language", "en"), ("format", "json")];
         let api_url = utils::urls::builder(base_url, params);
 
         let mut response = isahc::get(api_url).expect("Unable to send Location request");
@@ -31,7 +32,7 @@ impl Location for GeoLocation {
         let body = response.text().expect("Unable to read Location response body");
         let loc: GeoLocation = serde_json::from_str(&body).expect("Unable to parse Location JSON response");
 
-        let city = loc.results[0].admin2.to_owned();
+        let city = loc.results[0].name.to_owned();
         let country_code = loc.results[0].country_code.to_owned();
 
         LocationData {
@@ -39,7 +40,7 @@ impl Location for GeoLocation {
             country_code,
             latitude: loc.results[0].latitude,
             longitude: loc.results[0].longitude,
-            location: format!("{}, {}", loc.results[0].admin2, loc.results[0].country_code),
+            location: format!("{}, {}", loc.results[0].name, loc.results[0].country_code),
             created_at: utils::get_now(),
         }
     }
